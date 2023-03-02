a4bd7fdef17de7b83f5bba16082003d545f9642d
obicerj <obicerjestoni@gmail.com>
2023-03-02 11:39:00 +0800

    Add initial files for sidebar content
    Profile and Search people

---
+++ src/features/search/SearchPeople.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchPeople = () => {
+  return <div>SearchPeople</div>;
+};
+
+export default SearchPeople;
--- src/features/sidebar/SidebarContent.jsx
+++ src/features/sidebar/SidebarContent.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { ConvoContainer } from "../inbox";
+import Profile from "../profile/Profile";
+import SearchPeople from "../search/SearchPeople";
 
 const sidebarContent = () => {
   return (
     <aside className="relative border-r border-main w-full h-full md:w-32 md:min-w-[24rem] overflow-x-hidden">
-      <div>sidebarContent</div>
+      {/* List of convos / Messages */}
+      <ConvoContainer />
+
+      {/* Search */}
+      <SearchPeople />
+
+      {/* Profile */}
+      <Profile />
     </aside>
   );
 };
