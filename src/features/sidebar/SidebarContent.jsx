d5af0a34f1ad998404b905b13b304b67226f5818
obicerj <obicerjestoni@gmail.com>
2023-03-02 11:54:49 +0800

    Change background color of Sidebar content

--- src/features/sidebar/SidebarContent.jsx
+++ src/features/sidebar/SidebarContent.jsx
@@ -5,15 +5,20 @@ import SearchPeople from "../search/SearchPeople";
 
 const sidebarContent = () => {
   return (
-    <aside className="relative border-r border-main w-full h-full md:w-32 md:min-w-[24rem] overflow-x-hidden">
+    <aside className="
+    relative 
+    bg-slate-900 
+    border-slate-900
+    border-r border-main 
+    w-full h-full md:w-32 md:min-w-[24rem] overflow-x-hidden">
       {/* List of convos / Messages */}
       <ConvoContainer />
 
       {/* Search */}
-      <SearchPeople />
+      {/* <SearchPeople /> */}
 
       {/* Profile */}
-      <Profile />
+      {/* <Profile /> */}
     </aside>
   );
 };
