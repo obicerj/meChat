106ee6e9c6cc65d229bec89f2630cec47815758f
obicerj <obicerjestoni@gmail.com>
2023-03-03 22:08:00 +0800

    Create Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Input = ({ 
+  labelClass, 
+  labelText, 
+  inputType, 
+  inputName, 
+  inputID,
+  inputPlaceholder,
+  inputClass
+}) => {
+  return (
+    <>
+      <label className={`text-slate-300 font-medium ${labelClass}`}>{labelText}</label>
+      <input
+        type={inputType}
+        name={inputName}
+        id={inputID}
+        placeholder={inputPlaceholder}
+        className={`
+        border rounded-lg text-lg px-4 py-2.5
+        focus:outline-0
+        focus:ring-2
+        focus:ring-blue-400
+        ${inputClass}
+        `}
+      />
+    </>
+  );
+};
+
+export default Input;
