cc7d5f4589931b10466f26f5eff876bae1761cf6
obicerj <obicerjestoni@gmail.com>
2023-03-03 22:08:12 +0800

    Add design for login

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,5 +1,6 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
+import Input from "../../components/Input";
 
 const Login = ({ setIsAuth}) => {
   const [err, setErr] = useState(false);
@@ -17,53 +18,56 @@ const Login = ({ setIsAuth}) => {
     }
 }
   return (
-    <>
-      <div className="text-center">
-        <h1 className="text-3xl font-bold">Sign in to OChat</h1>
+    <div className="bg-slate-900">
+      <div className="w-2/5 mx-auto flex flex-col justify-center h-screen">
+        <div className="text-center ">
+          <h1 className="text-3xl font-bold">
+            <span className="text-blue-500">Me</span>
+            <span className="text-red-500">CHAT</span>
+          </h1>
+        </div>
+        <form onSubmit={handleLogin} className="flex flex-col gap-4 mt-8">
+          <div className="flex flex-col gap-2">
+            <Input
+              labelText={"Email"}
+              inputType={"email"}
+              inputName={"email"}
+              inputID={"email"}
+              inputPlaceholder={"e.g your-email@mechat.com"}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Input
+              labelText={"Password"}
+              inputType={"password"}
+              inputName={"password"}
+              inputID={"password"}
+              inputPlaceholder={"********"}
+            />
+          </div>
+
+          {err && 
+          <div className="bg-red-500 rounded-md text-slate-200 text-sm px-2 py-1.5">
+            <span>Something went wrong</span>
+          </div>
+          }
+
+          <button className="bg-green-500 font-bold text-lg text-white rounded-lg px-4 py-2.5">
+            Log in
+          </button>
+          <p className="text-slate-300">
+            Need an account?{" "}
+            <span
+              className="font-medium cursor-pointer text-indigo-300"
+              onClick={() => setIsAuth(false)}
+            >
+              Register
+            </span>
+          </p>
+        </form>
       </div>
-      <form onSubmit={handleLogin} className="flex flex-col gap-4 mt-8">
-        <div className="flex flex-col gap-2">
-          <label className="font-medium">Email</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="e.g your-email@ochat.com"
-            className="
-        border rounded-lg text-lg px-4 py-2.5
-        "
-          />
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <label className="font-medium">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="********"
-            className="
-        border rounded-lg text-lg px-4 py-2.5 
-        "
-          />
-        </div>
-
-        <span>{err && <span>Something went wrong</span>}</span>
-
-        <button className="bg-green-500 text-lg text-white rounded-lg px-4 py-2.5">
-          Next
-        </button>
-        <p>
-          Need an account?{" "}
-          <span
-            className="font-medium cursor-pointer"
-            onClick={() => setIsAuth(false)}
-          >
-            Register
-          </span>
-        </p>
-      </form>
-    </>
+    </div>
   );
 };
 
