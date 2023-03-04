9ea315b838a2fd23f02d9d22adc421b97b6600aa
obicerj <obicerjestoni@gmail.com>
2023-03-04 18:00:57 +0800

    Hide scrollbar

--- src/features/sidebar/SidebarContent.jsx
+++ src/features/sidebar/SidebarContent.jsx
@@ -15,7 +15,8 @@ const sidebarContent = () => {
     bg-slate-900 
     border-slate-900
     border-r border-main 
-    w-full h-full md:w-32 md:min-w-[24rem] overflow-x-hidden">
+    w-full h-full md:w-32 md:min-w-[24rem] overflow-x-hidden
+    overflow-y-scroll hide-scrollbar">
       {/* List of convos / Messages */}
       {sidebarContent  === "convos" && (
         <ConvoContainer />
