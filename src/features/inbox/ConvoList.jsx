743d9537bd33abf6136bda97f54ddb351381739d
obicerj <obicerjestoni@gmail.com>
2023-03-04 18:01:58 +0800

    Fix header layer with z-index

--- src/features/inbox/ConvoList.jsx
+++ src/features/inbox/ConvoList.jsx
@@ -33,7 +33,7 @@ const ConvoList = () => {
 
   return (
     <>
-      <div className="flex flex-col sticky top-0 bg-slate-900">
+      <div className="flex flex-col sticky top-0 bg-slate-900 z-10">
         {/* HEADER */}
         <header className="flex flex-row justify-between px-2 py-2 mt-2">
           <h2 className="text-2xl text-gray-300 mx-2">Chats</h2>
